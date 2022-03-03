7029ea05116a2a446f0683fe32823c1311c09e0f
gravesti <isaac.gravestock@roche.com>
2022-03-03 16:18:24 +0100

    radical rewrite

--- pkgdown_check.R
+++ pkgdown_check.R
@@ -1,27 +1,10 @@
 #!/usr/bin/env Rscript
 
-check_pkgdown_topics <- function() {
-  # Read in all the topics from _pkgdown.yaml.
-  pkg_yml <- yaml::yaml.load_file("_pkgdown.yaml")
-  pkgdown_topics <- unlist(lapply(pkg_yml$reference, \(x) x[["contents"]]))
-
-  rd_files <- dir("man", pattern = ".rd", ignore.case = TRUE)
-  missing <- lapply(rd_files, \(f) {
-    this_file <- tools::parse_Rd(file.path("man", f))
-    # Check if either the name or the alias is in the yaml.
-    names <- unlist(lapply(
-      this_file,
-      \(x) if (is.list(x) & attr(x, "Rd_tag") %in% c("\\name", "\\alias")) x[[1]])
-    )
-    if (!any(names %in% pkgdown_topics)) f else NULL
-  })
-  missing <- unique(unlist(missing))
-  if (is.null(missing)) {
-    print("Everything seems to be in _pkgdown.yaml! :)")
-  } else {
-    stop(paste0("Might be missing these topics: ", paste0(missing, collapse = ", ")))
-  }
-}
-
-
-check_pkgdown_topics()
+tryCatch(
+  {
+    pkgdown::build_reference_index()
+    pkgdown::build_articles_index()
+  },
+  error = function(e) e,
+  warning = function(w) stop(w)
+)
