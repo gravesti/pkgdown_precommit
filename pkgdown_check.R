fe10e85d2730c024ffcf02a50573a89f4426bbc3
gravesti <isaac.gravestock@roche.com>
2022-03-04 12:24:10 +0100

    warn-only arg

--- pkgdown_check.R
+++ pkgdown_check.R
@@ -1,4 +1,15 @@
-#!/usr/bin/env Rscript
+"Attempt to build {pkgodwn} indexes during a precommit.
+Usage:
+  pkgdown-index [--warn_only]
+Options:
+  --warn_only  Print warnings instead of blocking the commit. Should be
+               used with `verbose: True` in `.pre-commit-config.yaml`.
+               Otherwise, warnings will never be shown to the user.
+" -> doc
+
+arguments <- docopt::docopt(doc)
+
+error_fun <- if (arguments$warn_only) force else stop
 
 tryCatch(
   {
@@ -6,5 +17,5 @@ tryCatch(
     pkgdown::build_articles_index()
   },
   error = function(e) e,
-  warning = function(w) stop(w)
+  warning = function(w) error_fun(w)
 )
