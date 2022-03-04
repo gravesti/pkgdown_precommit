d8b7e9d80d62a75aa4cba5eccdc8be07e2bc69fd
gravesti <isaac.gravestock@roche.com>
2022-03-04 14:57:34 +0100

    as function

--- pkgdown_index.R
+++ pkgdown_index.R
@@ -1,11 +1,14 @@
 #!/usr/bin/env Rscript
 
-error_fun <- stop
-tryCatch(
-  {
-    pkgdown::build_reference_index()
-    pkgdown::build_articles_index()
-  },
-  error = function(e) e,
-  warning = function(w) error_fun(w)
-)
+check_pkgdown <- function() {
+  tryCatch(
+    {
+      pkgdown::build_reference_index()
+      pkgdown::build_articles_index()
+    },
+    error = function(e) e,
+    warning = function(w) stop(w)
+  )
+}
+
+check_pkgdown()
